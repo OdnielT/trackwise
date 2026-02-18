63c691d5d986eee2830072273a36ccdb6c50ddb3
OdnielT <odnielt28@gmail.com>
2026-02-18 15:56:16 +0000

    create form

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const AddTask = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const addTask = (e) => {
+    e.preventDefault()
+    console.log({title,description})
+  };
+
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Task Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Task Name" />
+        <Form.Control
+          type="text"
+          placeholder="Enter Task Name"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Task Description</Form.Label>
-        <Form.Control type="text" placeholder="Task Description" />
+        <Form.Control
+          type="text"
+          placeholder="Task Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </Form.Group>
 
       <div className="text-end">
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={(e) => addTask(e)}>
           Add Task
         </Button>
       </div>
