6070fd2605d49ad0f6c5b6eae7793f1d124d0229
OdnielT <odnielt28@gmail.com>
2026-02-19 16:41:07 +0000

    add update task compo

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,44 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
+import MyVerticallyCenteredModal from "./UpdateTask";
 
 const TaskList = () => {
-
+  
   const updateTask = () => {
-    console.log('Updaet Task')
-  }
+    console.log("Updaet Task");
+    setModalShow(true);
+  };
 
   const deleteTask = () => {
-    console.log('Delete Task')
-  }
+    console.log("Delete Task");
+  };
+
+  const [modalShow, setModalShow] = useState(false);
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr className="text-center">
-          <th>#</th>
-          <th>Task</th>
-          <th>Description</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="text-center">
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>
-            <Button variant="primary" className="mx-2" onClick={() => updateTask()}>
-              <i className="bi bi-pencil-square"></i>
-            </Button>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr className="text-center">
+            <th>#</th>
+            <th>Task</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-center">
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>
+              <Button
+                variant="primary"
+                className="mx-2"
+                onClick={() => updateTask()}
+              >
+                <i className="bi bi-pencil-square"></i>
+              </Button>
 
-            <Button variant="primary" onClick={() => deleteTask()}>
-              <i className="bi bi-trash3-fill"></i>
-            </Button>
-          </td>
-        </tr>
-      </tbody>
-    </Table>
+              <Button variant="primary" onClick={() => deleteTask()}>
+                <i className="bi bi-trash3-fill"></i>
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
   );
 };
 
